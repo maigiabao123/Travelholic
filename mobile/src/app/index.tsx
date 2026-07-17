d67354a5e50a1ed6a86e09f52b02f76db72e4ca4
maigiabao123 <maigiabao22082010@gmail.com>
2026-07-17 11:40:17 +0700

    second in 17/7

--- mobile/src/app/index.tsx
+++ mobile/src/app/index.tsx
@@ -1,2 +1,49 @@
-import LoginScreen from '../screens/auth/LoginScreen';
-export default LoginScreen;
+// app/index.tsx
+import React, { useEffect, useState } from 'react';
+import { ActivityIndicator, View } from 'react-native';
+import { useRouter } from 'expo-router';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+export default function Index() {
+  const router = useRouter();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const checkToken = async () => {
+      try {
+        const token = await AsyncStorage.getItem('token');
+
+        if (token) {
+          router.replace('/');
+        } else {
+
+          router.replace('/login');
+        }
+      } catch (err) {
+        console.warn('Error reading token', err);
+        router.replace('/login');
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    checkToken();
+  }, [router]);
+
+
+  if (loading) {
+    return (
+      <View
+        style={{
+          flex: 1,
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+      >
+        <ActivityIndicator size="large" />
+      </View>
+    );
+  }
+
+  return null; 
+}
